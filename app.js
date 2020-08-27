f5d4a4bd8429fc95b257235645f8c5951ad397bb
HarryKay <kay.harry.j@gmail.com>
2020-08-27 17:23:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Creating function for Data plotting (Bar, gauge, bubble)
 function getPlot(id) {
     // getting data from the json file
-    d3.json("Data/samples.json").then((data)=> {
+    d3.json("data/samples.json").then((data)=> {
         console.log(data)
   
         var wfreq = data.metadata.map(d => d.wfreq)
