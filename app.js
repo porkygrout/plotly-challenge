3f9b548025a6d1fa7f77589c7679cf7fae2a4e68
HarryKay <kay.harry.j@gmail.com>
2020-08-27 18:37:21 -0400

    correcting stupid error

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ function getPlot(id) {
 // create the function to get the necessary data
 function getInfo(id) {
     // read the json file to get data
-    d3.json("Data/samples.json").then((data)=> {
+    d3.json("data/samples.json").then((data)=> {
         
         // get the metadata info for the demographic panel
         var metadata = data.metadata;
@@ -155,7 +155,7 @@ function init() {
     var dropdown = d3.select("#selDataset");
 
     // read the data 
-    d3.json("Data/samples.json").then((data)=> {
+    d3.json("data/samples.json").then((data)=> {
         console.log(data)
 
         // get the id data to the dropdwown menu
